39be88a8ab8b691cd99d4cb5093f1c69a9425c0b
Himanshu Budhia <budhia.himanshu96@gmail.com>
2019-06-15 19:01:41 -0400

    Solved left rotation

---
+++ Practice/Hackerrank/Arrays_LeftRotation.cpp
@@ -0,0 +1,106 @@
+/* https://www.hackerrank.com/challenges/ctci-array-left-rotation/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=arrays */
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long int
+#define mod 1000000007
+void check(ll a) { cout<<a<<endl; }
+bool prime [1000001];
+int sieve(int a){memset(prime,true,sizeof(prime));int p;int i;for(p=2;p*p<=a;p++){if(prime[p]==true){for(i=p*2;i<=a;i+=p)prime[i]=false;}}return 0;}
+//vector<ll> a
+//vector<pair<ll,ll> > a
+
+vector<string> split_string(string);
+
+// Complete the rotLeft function below.
+vector<int> rotLeft(vector<int> a, int d) {
+	   vector<int> ans;
+	   int n = a.size();
+	   int j=0;
+	   for(int i=0;i<n;++i){
+	   		if(d<n){
+	   			ans.push_back(a.at(d));
+				d++;	   			
+			   }
+	   		else{
+	   			ans.push_back(a.at(j));
+				j++;	   						   	
+			   }
+	   }
+	   return ans;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string nd_temp;
+    getline(cin, nd_temp);
+
+    vector<string> nd = split_string(nd_temp);
+
+    int n = stoi(nd[0]);
+
+    int d = stoi(nd[1]);
+
+    string a_temp_temp;
+    getline(cin, a_temp_temp);
+
+    vector<string> a_temp = split_string(a_temp_temp);
+
+    vector<int> a(n);
+
+    for (int i = 0; i < n; i++) {
+        int a_item = stoi(a_temp[i]);
+
+        a[i] = a_item;
+    }
+
+    vector<int> result = rotLeft(a, d);
+
+    for (int i = 0; i < result.size(); i++) {
+        fout << result[i];
+
+        if (i != result.size() - 1) {
+            fout << " ";
+        }
+    }
+
+    fout << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
+
