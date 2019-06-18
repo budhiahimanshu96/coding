fac58468581c18028533ba77220e43ebe1cbfe27
Himanshu Budhia <budhia.himanshu96@gmail.com>
2019-06-17 21:56:41 -0400

    Started working on greedy question

---
+++ Practice/Hackerrank/Luck Balance.cpp
@@ -0,0 +1,85 @@
+/* https://www.hackerrank.com/challenges/luck-balance/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=greedy-algorithms */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+#define ll long long int
+#define mod 1000000007
+void check(ll a) { cout<<a<<endl; }
+bool prime [1000001];
+int sieve(int a){memset(prime,true,sizeof(prime));int p;int i;for(p=2;p*p<=a;p++){if(prime[p]==true){for(i=p*2;i<=a;i+=p)prime[i]=false;}}return 0;}
+//vector<ll> a
+//vector<pair<ll,ll> > a
+
+vector<string> split_string(string);
+
+// Complete the luckBalance function below.
+int luckBalance(int k, vector<vector<int>> contests) {
+
+
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string nk_temp;
+    getline(cin, nk_temp);
+
+    vector<string> nk = split_string(nk_temp);
+
+    int n = stoi(nk[0]);
+
+    int k = stoi(nk[1]);
+
+    vector<vector<int>> contests(n);
+    for (int i = 0; i < n; i++) {
+        contests[i].resize(2);
+
+        for (int j = 0; j < 2; j++) {
+            cin >> contests[i][j];
+        }
+
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
+
+    int result = luckBalance(k, contests);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
+
